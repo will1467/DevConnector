6fcc203a7b3ac3d0e223432fa567035dbffce483
William Wrigley <wwrigley@csc.com>
2019-10-25 16:08:51 +1300

    set up db conn and basic routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.get("/", (req, res) => {
     res.send("API running");
 });
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
